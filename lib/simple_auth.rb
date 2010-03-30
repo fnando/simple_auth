2fb68c1dca3a1bb9dd615b895f165b4e4a884634
Nando Vieira <fnando.vieira@gmail.com>
2010-03-30 12:28:08 -0300

    Bug fix: missing requirement was raising error when using it as gem

--- lib/simple_auth.rb
+++ lib/simple_auth.rb
@@ -1,5 +1,6 @@
 require "digest/sha2"
 require "simple_auth/config"
+require "simple_auth/action_controller"
 require "simple_auth/active_record"
 require "simple_auth/session"
 require "simple_auth/version"
@@ -8,11 +9,11 @@ module SimpleAuth
   class NotAuthorized < Exception; end
 end
 
-ActiveRecord::Base.send :include, SimpleAuth::ActiveRecord::InstanceMethods
-ActiveRecord::Base.send :extend, SimpleAuth::ActiveRecord::ClassMethods
+::ActiveRecord::Base.send :include, SimpleAuth::ActiveRecord::InstanceMethods
+::ActiveRecord::Base.send :extend, SimpleAuth::ActiveRecord::ClassMethods
 
-ActionController::Base.send :include, SimpleAuth::ActionController::Implementation
-ActionController::Base.send :include, SimpleAuth::ActionController::InstanceMethods
-ActionController::Base.send :extend, SimpleAuth::ActionController::ClassMethods
+::ActionController::Base.send :include, SimpleAuth::ActionController::Implementation
+::ActionController::Base.send :include, SimpleAuth::ActionController::InstanceMethods
+::ActionController::Base.send :extend, SimpleAuth::ActionController::ClassMethods
 
 I18n.load_path += Dir[File.dirname(__FILE__) + "/../config/locales/*.yml"]
